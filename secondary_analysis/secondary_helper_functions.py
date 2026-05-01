890522d4bbeb007e77c5f0b78cffcdb8ffbe95eb
MSleeper1 <sleeper.meghan@gmail.com>
2026-04-30 23:37:37 -0700

    reduced required columns for sample_info tsv

--- secondary_analysis/secondary_helper_functions.py
+++ secondary_analysis/secondary_helper_functions.py
@@ -6,8 +6,8 @@ import pandas as pd
 def get_sample_info_df(input_tsv):
     sample_info = pd.read_table(input_tsv, dtype=str)
     # Verify column names
-    if not {'ref', 'patient_id', 'srx_id', 'accession', 'layout', 'group', 'age', 'sex', 'ethnicity'}.issubset(sample_info.columns.values):
-        raise KeyError("The sample info file must contain the following named columns: 'ref', 'patient_id', 'srx_id', 'accession', 'layout', 'group', 'age', 'sex' 'ethnicity'")
+    if not {'ref', 'patient_id', 'srx_id', 'accession', 'layout', 'group'}.issubset(sample_info.columns.values):
+        raise KeyError("The sample info file must contain the following named columns: 'ref', 'patient_id', 'srx_id', 'accession', 'layout', 'group'")
     sample_info = sample_info.set_index(["srx_id"], drop=False)
     return sample_info
 
